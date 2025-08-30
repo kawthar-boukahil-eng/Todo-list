55c997bdc1cda410d6b4c21c889df05f28dcc282
KawtharDIDI <kawthar.boukahil@univ-constantine2.dz>
2025-08-30 20:03:56 +0100

    add popup

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnadd = document.getElementById("add-task")
+const inputpop = document.getElementById("pop")
+const submit = document.getElementById("submit")
+const popup = document.getElementById("absolute")
+btnadd.addEventListener("click", function(){
+    popup.classList.add("show");
+    
+})
+submit.addEventListener("click",function(){
+    
+})
